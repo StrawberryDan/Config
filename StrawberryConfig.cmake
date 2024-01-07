22fcc43fdd2fd31413a3f8dfc44e3be8c9d6f035
StrawberryDan <DanRhysMorris@gmail.com>
2024-01-07 16:18:05 +0000

    Add Build Configuration definitions

---
+++ StrawberryConfig.cmake
@@ -0,0 +1,3 @@
+add_compile_definitions($<$<CONFIG:Debug>:STRAWBERRY_DEBUG=1>)
+add_compile_definitions($<$<CONFIG:RelWithDebInfo>:STRAWBERRY_RELEASE=1>)
+add_compile_definitions($<$<CONFIG:Release>:STRAWBERRY_RELEASE=1 STRAWBERRY_FINAL=1>)
