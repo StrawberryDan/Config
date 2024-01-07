87b76267ff5c8c32494c7fc38266c3c22b382747
StrawberryDan <DanRhysMorris@gmail.com>
2024-01-07 16:57:54 +0000

    Add target platform macros

--- StrawberryConfig.cmake
+++ StrawberryConfig.cmake
@@ -1,3 +1,8 @@
 add_compile_definitions("$<$<CONFIG:Debug>:STRAWBERRY_DEBUG=1>")
 add_compile_definitions("$<$<CONFIG:RelWithDebInfo>:STRAWBERRY_RELEASE=1>")
 add_compile_definitions("$<$<CONFIG:Release>:STRAWBERRY_RELEASE=1 STRAWBERRY_FINAL=1>")
+
+
+add_compile_definitions("$<$<PLATFORM_ID:Windows>:STRAWBERRY_TARGET_WINDOWS>")
+add_compile_definitions("$<$<PLATFORM_ID:Darwin>:STRAWBERRY_TARGET_MAC>")
+add_compile_definitions("$<$<PLATFORM_ID:Linux>:STRAWBERRY_TARGET_LINUX>")
