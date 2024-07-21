572d0acf892ba55d3c6ee47acce7787cc7178e6d
StrawberryDan <DanRhysMorris@gmail.com>
2024-07-21 12:56:12 +0100

    Create `new_strawberry_executable` and `new_strawberry_library` functions.

--- StrawberryConfig.cmake
+++ StrawberryConfig.cmake
@@ -1,8 +1,24 @@
-add_compile_definitions("$<$<CONFIG:Debug>:STRAWBERRY_DEBUG=1>")
-add_compile_definitions("$<$<CONFIG:RelWithDebInfo>:STRAWBERRY_RELEASE=1>")
-add_compile_definitions("$<$<CONFIG:Release>:STRAWBERRY_RELEASE=1 STRAWBERRY_FINAL=1>")
+function(add_strawberry_definitions TARGET)
+    target_compile_definitions(${TARGET} "$<$<CONFIG:Debug>:STRAWBERRY_DEBUG=1>")
+    target_compile_definitions(${TARGET} "$<$<CONFIG:RelWithDebInfo>:STRAWBERRY_RELEASE=1>")
+    target_compile_definitions(${TARGET} "$<$<CONFIG:Release>:STRAWBERRY_RELEASE=1 STRAWBERRY_FINAL=1>")
 
 
-add_compile_definitions("$<$<PLATFORM_ID:Windows>:STRAWBERRY_TARGET_WINDOWS>")
-add_compile_definitions("$<$<PLATFORM_ID:Darwin>:STRAWBERRY_TARGET_MAC>")
-add_compile_definitions("$<$<PLATFORM_ID:Linux>:STRAWBERRY_TARGET_LINUX>")
+    target_compile_definitions(${TARGET} "$<$<PLATFORM_ID:Windows>:STRAWBERRY_TARGET_WINDOWS>")
+    target_compile_definitions(${TARGET} "$<$<PLATFORM_ID:Darwin>:STRAWBERRY_TARGET_MAC>")
+    target_compile_definitions(${TARGET} "$<$<PLATFORM_ID:Linux>:STRAWBERRY_TARGET_LINUX>")
+endfunction()
+
+
+function(new_strawberry_executable NAME SOURCE_FILES)
+    add_executable(${NAME} ${SOURCE_FILES})
+    add_strawberry_definitions(${NAME})
+    set_target_properties(${NAME} PROPERTIES CXX_STANDARD 23)
+endfunction()
+
+
+function(new_strawberry_library NAME SOURCE_FILES)
+    add_library(${NAME} STATIC ${SOURCE_FILES})
+    add_strawberry_definitions(${NAME})
+    set_target_properties(${NAME} PROPERTIES CXX_STANDARD 23)
+endfunction()
